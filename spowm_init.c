1ee15ca0947a3ff67ae01b00d099a5c2b73a7494
esseivao <olivier.esseiva@post.ch>
2026-04-06 15:21:27 +0200

    Validate exponentiation table parameters

--- spowm_init.c
+++ spowm_init.c
@@ -17,9 +17,20 @@
  * along with GMPMEE. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <limits.h>
 #include <stdlib.h>
 #include <gmp.h>
 #include "gmpmee.h"
+
+static void
+validate_block_width(size_t block_width)
+{
+  if (block_width == 0
+      || block_width >= (size_t) (sizeof(int) * CHAR_BIT - 1))
+    {
+      abort();
+    }
+}
 
 void
 gmpmee_spowm_init(gmpmee_spowm_tab table, size_t len, mpz_t modulus,
@@ -28,13 +39,16 @@ gmpmee_spowm_init(gmpmee_spowm_tab table, size_t len, mpz_t modulus,
   size_t i, j;
   size_t tab_len;  /* Size of a subtable. */
   mpz_t *t;        /* Temporary variable for subtable. */
+  size_t effective_block_width;
 
   table->len = len;
-  table->block_width = block_width;
-  if (len < block_width) {
-    table->block_width = len;
+  effective_block_width = block_width;
+  if (len < effective_block_width) {
+    effective_block_width = len;
   }
-  table->tabs_len = (len + block_width - 1) / block_width;
+  validate_block_width(effective_block_width);
+  table->block_width = effective_block_width;
+  table->tabs_len = (len + effective_block_width - 1) / effective_block_width;
 
   mpz_init(table->modulus);
   mpz_set(table->modulus, modulus);
@@ -42,17 +56,19 @@ gmpmee_spowm_init(gmpmee_spowm_tab table, size_t len, mpz_t modulus,
   /* Allocate and initialize space for pointers to tables. */
   table->tabs = (mpz_t **)malloc(table->tabs_len * sizeof(mpz_t *));
 
-  tab_len = 1 << block_width;
+  tab_len = 1 << effective_block_width;
   for (i = 0; i < table->tabs_len; i++)
     {
 
       /* Last block may be more narrow than the other, but they are
          never zero. */
       if (i == table->tabs_len - 1
-	  && len - (table->tabs_len - 1) * block_width < block_width)
+	  && len - (table->tabs_len - 1) * effective_block_width
+          < effective_block_width)
 	{
-	  block_width = len - (table->tabs_len - 1) * block_width;
-	  tab_len = 1 << block_width;
+	  effective_block_width = len - (table->tabs_len - 1)
+            * effective_block_width;
+	  tab_len = 1 << effective_block_width;
 	}
 
       /* Allocate and initialize a table. */
